320f571496743128b05dd0d33e77ff84806cdf91
chenglei <chenglei@4366.com>
2020-09-27 10:29:06 +0800

    打包成 可运行jar包

---
+++ persist-user/src/main/java/com/jcpl/persist/exception/MyErrorAttributes.java
@@ -0,0 +1,24 @@
+package com.jcpl.persist.exception;
+
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
+
+/**
+ * @author Administrator
+ */
+@Component
+public class MyErrorAttributes extends DefaultErrorAttributes {
+    @Override
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace) {
+        Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, includeStackTrace);
+        errorAttributes.remove("timestamp");
+        errorAttributes.remove("path");
+        errorAttributes.remove("timestamp");
+        errorAttributes.remove("error");
+        errorAttributes.put("data", "");
+        return errorAttributes;
+    }
+}
