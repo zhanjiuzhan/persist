2bf19d089deb468ab8331ac30fe5e5bb38804dc8
chenglei <chenglei@4366.com>
2020-09-29 16:04:06 +0800

    controller 请求的log日志打印

--- persist-user/src/main/java/com/jcpl/persist/reqlog/ControllerLogAspect.java
+++ persist-user/src/main/java/com/jcpl/persist/reqlog/ControllerLogAspect.java
@@ -1,5 +1,6 @@
 package com.jcpl.persist.reqlog;
 
+import org.apache.tomcat.util.buf.StringUtils;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -8,7 +9,9 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
 /**
@@ -27,10 +30,46 @@ public class ControllerLogAspect {
     public void beforePointcut(JoinPoint joinPoint) {
         MethodSignature methodSignature =  (MethodSignature) joinPoint.getSignature();
         Method method = methodSignature.getMethod();
-        //RequestMapping annotation = method.getAnnotation(RequestMapping.class);
-        //String[] paths = annotation.value();
+        Annotation annotation = method.getAnnotation(RequestMapping.class);
+        if (annotation == null) {
+            annotation = method.getAnnotation(GetMapping.class);
+        }
+        if (annotation == null) {
+            annotation = method.getAnnotation(PostMapping.class);
+        }
+        if (annotation == null) {
+            annotation = method.getAnnotation(PutMapping.class);
+        }
+        if (annotation == null) {
+            annotation = method.getAnnotation(DeleteMapping.class);
+        }
+        if (annotation == null) {
+            annotation = method.getAnnotation(PatchMapping.class);
+        }
+
+        String[] paths = null;
+        Class clazz = annotation.annotationType();
+        if (clazz == RequestMapping.class) {
+            paths = ((RequestMapping)annotation).value();
+        }
+        if (clazz == GetMapping.class) {
+            paths = ((GetMapping)annotation).value();
+        }
+        if (clazz == PostMapping.class) {
+            paths = ((PostMapping)annotation).value();
+        }
+        if (clazz == PutMapping.class) {
+            paths = ((PutMapping)annotation).value();
+        }
+        if (clazz == DeleteMapping.class) {
+            paths = ((DeleteMapping)annotation).value();
+        }
+        if (clazz == PatchMapping.class) {
+            paths = ((PatchMapping)annotation).value();
+        }
+
         Object[] args = joinPoint.getArgs();
-        //logger.info("请求路径为: " + StringUtils.join(paths));
+        logger.info("请求路径为: " + StringUtils.join(paths));
         StringBuilder par = new StringBuilder();
         for (Object tmp : args) {
             par.append(tmp.toString());
